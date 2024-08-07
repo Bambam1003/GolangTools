370bc594c8ad4be71749b53c25cbdaf9c3824036
Bambam <Bambam1003@163.com>
2024-08-07 23:28:05 +0800

    base code

---
+++ leetcode/base_question.go
@@ -0,0 +1,13 @@
+package leetcode
+
+// twoSum 两数之和
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, num := range nums {
+		if j, ok := m[target-num]; ok {
+			return []int{j, i}
+		}
+		m[num] = i
+	}
+	return []int{}
+}
